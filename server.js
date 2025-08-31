de4ef4be33e84817e2d5967ebf3f44408530f4b6
daikiImai <8ikiad@gmail.com>
2025-08-31 22:46:34 +0900

    improve in proxy environment

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set('trust proxy', 1);
+
 /** --- セッション --- **
  *  - connect-pg-simple に pg Pool を渡してセッションを DB に保存
  */
